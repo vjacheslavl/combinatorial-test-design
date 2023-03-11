529b43dcc8f7c02e957a615da58f98ec13e141b7
Vjaceslavs Lukasevics <vjaceslavs.lukasevics@agcsoft.com>
2023-03-11 21:20:40 +0200

    Rework db model design

--- src/client/Components/Attributes/Values/ValueItem.tsx
+++ src/client/Components/Attributes/Values/ValueItem.tsx
@@ -1,39 +1,36 @@
-import React, { useState } from "react";
-import { ValueResponse } from "../../../../server/domain/TestDesignResponse";
-import { Put } from "../../../Services";
-import { apiRoute } from "../../../utils";
+import React, { useState } from 'react';
+import { ValueResponse } from '../../../../server/domain/TestDesignResponse';
+import { Put } from '../../../Services';
+import { apiRoute } from '../../../utils';
 
-const valueTypes = ["positive","happyPath", "negative"]
+const valueTypes = ['positive', 'happyPath', 'negative'];
 
 const ValueItem: React.FC<{
-      content: ValueResponse
-}> = (props) => {
+	content: ValueResponse;
+}> = props => {
+	const [valueType, setValueType] = useState(valueTypes.indexOf(props.content.type));
 
-      const [valueType, setValueType] = useState(valueTypes.indexOf(props.content.type));
+	const setValueTypeHandler = async (): Promise<void> => {
+		const changedValue = valueType + 1 == 3 ? 0 : valueType + 1;
+		try {
+			await Put(apiRoute.getRoute('values'), {
+				_id: props.content._id,
+				attributeId: props.content.attributeId,
+				name: props.content.name,
+				type: valueTypes[changedValue]
+			});
+			setValueType(changedValue);
+                  console.log(changedValue);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
-      const setValueTypeHandler = async (
-      ): Promise<void> => {
-            const changedValue = valueType +1 == 3? 0 :valueType +1
-            try {
-                  await Put(
-                        apiRoute.getRoute("values"),
-                        {
-                              _id: props.content._id,
-                              attributeId: props.content.attributeId,
-                              name: props.content.name,
-                              type: valueTypes[changedValue]
-                        }
-                  );
-                  setValueType(changedValue);
-
-            } catch (e) {
-                  console.log(e);
-            }
-      };
-
-      return (
-            <div className={`valueItem ${valueTypes[valueType]}`} onClick={setValueTypeHandler}>{props.content.name}</div>
-      );
+	return (
+		<div className={`valueItem ${valueTypes[valueType]}`} onClick={setValueTypeHandler}>
+			{props.content.name}
+		</div>
+	);
 };
 
 export default ValueItem;
